3d65e642a2e67945e3aa5513090e2bf6fa7b0aed
kazuki <kazu0513taka0524@gmail.com>
2025-08-15 02:19:36 +0900

    Create chapter8_price_calc.py

---
+++ chapter8_price_calc.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+# 値段の計算（演算子の活用）
+
+apple_price = 120   # りんご1個の値段
+apple_count = 3     # 買う個数
+
+total_price = apple_price * apple_count  # 合計金額の計算
+
+print("りんごを" + str(apple_count) + "個買いました")
+print("合計金額は" + str(total_price) + "円です")
